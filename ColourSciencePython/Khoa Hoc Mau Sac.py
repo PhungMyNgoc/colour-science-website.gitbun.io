3e434d15c5e718f0d1134f9a33459e5aee31d5b0
PhungMyNgoc <22158073@student.hcmute.edu.vn>
2025-05-18 19:02:17 +0700

    Add files via upload

---
+++ ColourSciencePython/Khoa Hoc Mau Sac.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Khoa học Màu sắc", layout="wide"
+)
+st.write("## Chào mừng bạn đến với project Khoa học Màu sắc của mình!")
+st.write('# Mình tên là Phùng Mỹ Ngọc, MaSV 22158073')
